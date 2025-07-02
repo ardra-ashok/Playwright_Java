7a1c39e553fe481be494d1c5f55527e84f005c56
ardra-ashok <ardraasok@yahoo.in>
2025-07-02 16:54:15 +1000

    Initial commit - Maven project

---
+++ src/main/java/org/example/firstExample.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import com.microsoft.playwright.*;
+
+import java.awt.*;
+
+public class firstExample {
+    public static void main(String[] args) throws InterruptedException {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        BrowserContext browserContext = browser.newContext(new Browser.NewContextOptions().setViewportSize(1512,982));
+        Page page = browserContext.newPage();
+        page.navigate("http://way2automation.com");
+        System.out.println(page.title());
+        Thread.sleep(2000);
+        page.close();
+        playwright.close();
+
+    }
+}
